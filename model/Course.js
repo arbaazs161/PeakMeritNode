3e2fed9dc0a3406181f783cf6b1e317a8dfc2ce6
arbaazs161 <arbaazs161@gmail.com>
2021-05-06 20:42:32 +0530

    Implemented Course Module. Author can add course.

--- model/Course.js
+++ model/Course.js
@@ -1,4 +1,4 @@
-const { Mongoose } = require("mongoose")
+const mongoose = require("mongoose")
 
 const courseSchema = {
     name: {
@@ -14,6 +14,11 @@ const courseSchema = {
     publishDate: {
         type: Date,
         default: Date.now
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author',
+        required: true
     }
 }
 
--- routes/course.js
+++ routes/course.js
@@ -1,18 +1,88 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Course = require('../model/Course');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+const path = require('path');
+
+router.use(bodyParser.urlencoded({extended: false}));
+
+mongoose.connect('mongodb://localhost/peakmerit')
+    .then(() => console.log('Connected to MongoDB..'))
+    .catch(err => console.err('Could not connect to MongoDB...', err.message));
 
 router.get('/:id?/:tag?/:name?', (req, res) => {
     var id = req.query.id;
     var tag = req.query.tag;
     var name = req.query.name;
 
-    console.log(`Id : ${id}, tag : ${tag}, Name: ${name}`);
-    
-    //res.send(course);
+    const courses = getAllCourses();
+
+    if(!id && !tag && !name){
+        courses.then(data => {
+            res.render('course', {title: 'Courses', headData: 'Add Course', message: '', error: '', courses: data});
+        });
+        
+    }
+    else{
+        res.write('Not Found!');
+    }
 });
 
 router.post('/', (req, res) =>{
-    console.log('Posted a Course');
+    
+    console.log(req.body);
+    var name = req.body.name;
+    var tagString = req.body.tags;
+    var author = '6093c81ef63cbaec6b6e94c0';
+
+    
+    
+    console.log(name);
+    console.log(tagString);
+    var tags = tagString.split(" ");
+    console.log(tags);
+
+    const result = addCourse(name, tags, author);
+    const courses = getAllCourses();
+    result.then(data => {
+        createDirectory(name);
+        courses.then(courseData => {
+            
+            res.render('course', {title: 'Courses', headData: 'Add Course', message: data.message, error: '', courses: courseData});
+        });
+    })
+    .catch(err => {
+        res.render('course', {title: 'Courses', headData: 'Add Course', message: '', error: err.message, courses: courses});
+    });
+    
 });
+
+async function addCourse(name, tags, author){
+    const course = new Course({
+        name: name,
+        tags: tags,
+        author: author
+    });
+
+    const result = await course.save();
+    console.log(result.message);
+    return result;
+}
+
+async function getAllCourses(){
+    const courses = await Course.find().select();
+    return courses;
+}
+
+function createDirectory(name){
+    fs.mkdir(path.join(__dirname, '../public/Arbaaz/'+name), (err) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Directory created successfully!');
+    });
+}
 
 module.exports = router;
